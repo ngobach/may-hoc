e1d4169e22cf5d1092554ef7a1d166225cd1b086
Ngô Xuân Bách <thanbaiks@gmail.com>
2016-06-11 14:17:21 +0700

    Fix splash

--- MayHoc/Program.cs
+++ MayHoc/Program.cs
@@ -16,8 +16,8 @@ namespace MayHoc
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            //Application.Run(new FormSplash());
-            Application.Run(new FormMain());
+            Application.Run(new FormSplash());
+            //Application.Run(new FormMain());
         }
     }
 }
